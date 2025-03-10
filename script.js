ff3aa0332faef526db340d9c5e1719cb278a8fa7
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-03-10 15:55:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
-  // your code here
+       let trimmedStr = str.trim();
+    let spaceIndex = trimmedStr.indexOf(" ");
+
+    // If no space is found, return the entire string
+    return spaceIndex === -1 ? trimmedStr : trimmedStr.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
